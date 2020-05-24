c9b36a375a7a8901e554192604ee401a0b7a7d00
Bharat Makhija <bhmakhij@microsoft.com>
2020-05-24 16:01:41 +0530

    CalculatorApplication - webAPI communicating to CalculatorService via ServiceRemoting

---
+++ CalculatorService.Interface/ICalculatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+
+namespace CalculatorService.Interface
+{
+    public interface ICalculatorService : IService
+    {
+        Task<int> Add(int a, int b);
+        Task<int> Substract(int a, int b);
+
+        Task<int> Multiply(int a, int b);
+
+        Task<int> Divide(int a, int b);
+    }
+}
---
+++ Web2/Controllers/ValueController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using CalculatorService.Interface;
+
+/// <summary>
+/// ValueController.cs
+/// </summary>
+namespace Web2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ValueController
+    {
+
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+            new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Add(a, b).Result;
+        }
+
+        [HttpGet]
+        public int Substract(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+            new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Substract(a, b).Result;
+        }
+
+
+        [HttpGet]
+        public int Multiply(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+            new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Multiply(a, b).Result;
+        }
+
+
+        [HttpGet]
+        public int Divide(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+            new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Divide(a, b).Result;
+        }
+    }
+}
